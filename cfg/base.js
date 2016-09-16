369aeaa902ae882e8956b1593b1ae196936d8465
Emilien <emilien_muraton@yahoo.fr>
2016-09-16 10:55:20 +0200

    Add webpack conf.

---
+++ cfg/base.js
@@ -0,0 +1,63 @@
+var path = require('path');
+var port = 8000;
+var srcPath = path.join(__dirname, '/../src');
+var publicPath = '/assets/';
+
+module.exports = {
+  port: port,
+  debug: true,
+  output: {
+    path: path.join(__dirname, '/../dist/assets'),
+    filename: 'app.js',
+    publicPath: publicPath
+  },
+  devServer: {
+    contentBase: './src/',
+    historyApiFallback: true,
+    hot: true,
+    port: port,
+    publicPath: publicPath,
+    noInfo: false
+  },
+  resolve: {
+    extensions: [
+      '',
+      '.js',
+      '.jsx'
+    ],
+    alias: {
+      actions: srcPath + '/actions/',
+      reducers: srcPath + '/reducers/',
+      components: srcPath + '/components/',
+      containers: srcPath + '/containers/',
+      styles: srcPath + '/styles/',
+      config: srcPath + '/config/' + process.env.REACT_WEBPACK_ENV
+    }
+  },
+  module: {
+    preLoaders: [{
+        test: /\.(js|jsx)$/,
+        include: path.join(__dirname, 'src'),
+        loader: 'eslint-loader'
+      }],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader'
+      },
+      {
+        test: /\.styl/,
+        loader: 'style-loader!css-loader!postcss-loader!stylus-loader'
+      },
+      {
+        test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=8192'
+      },
+        { test: /\.json$/, loader: "json-loader" },
+
+    ]
+  },
+  postcss: function () {
+    return [];
+  }
+};
